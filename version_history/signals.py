595aeb3ab728eed935e9cfdde777dbd1fc2f10be
Andrew Aikman <a.aikman@ic.ac.uk>
2018-05-23 17:35:39 +0100

    Serialization and deserialization of the plugin is succesful although still unable to store in the db

--- version_history/signals.py
+++ version_history/signals.py
@@ -7,6 +7,18 @@ from django.core.serializers import serialize, deserialize
 from .models import Version_History
 
 
+"""
+
+Create the STB plugin!!!
+
+Questions:
+    The plugin architecture is very confusing to follow
+    Is it best to use the plugin pool and serialize that way or track the DB tables down and duplicate them.
+    No way to serialize the plugin instance models
+    - When will the plugin pool docs be populated: http://docs.django-cms.org/en/latest/reference/plugins.html
+
+"""
+
 
 # Taken from cms/models/pluginmodel -> CMSPlugin
 from cms.plugin_pool import plugin_pool
@@ -23,9 +35,33 @@ def get_title(page, language):
         return None
 
 from django.utils.encoding import force_text
-from djangocms_history.helpers import get_plugin_data
 from djangocms_history.utils import get_plugin_model
 
+
+from djangocms_history.utils import get_plugin_fields
+# Taken from nad modified djangocms_history.helpers import get_plugin_data
+def get_plugin_data(plugin, only_meta=False):
+    if only_meta:
+        custom_data = None
+    else:
+        plugin_fields = get_plugin_fields(plugin.plugin_type)
+
+        # Edited
+        _plugin_data = serialize('python', (plugin,), fields=plugin_fields)[0]
+        #_plugin_data = serialize('json', (plugin,), fields=plugin_fields)
+
+
+        custom_data = _plugin_data['fields']
+
+    plugin_data = {
+        'pk': plugin.pk,
+        'creation_date': plugin.creation_date,
+        'position': plugin.position,
+        'plugin_type': plugin.plugin_type,
+        'parent_id': plugin.parent_id,
+        'data': custom_data,
+    }
+    return plugin_data
 
 def _publish_receiver(sender, **kwargs):
     #logic goes here
@@ -54,16 +90,20 @@ def _publish_receiver(sender, **kwargs):
             fetched_data = get_plugin_data(plugin=plugin)
 
             current_plugin_model = get_plugin_model(plugin.plugin_type)
+            data_model = force_text(current_plugin_model._meta)
 
             data = {
-                'model': force_text(current_plugin_model._meta),
-                'fields': fetched_data,
+                'model': data_model,
+                'fields': fetched_data['data'],
             }
 
             deserialized = list(deserialize('python', [data]))[0]
 
 
-            plugin_instance_list.append(fetched_data)
+            #deserialized_plugin = list(serialize('json', [current_plugin_model]))
+
+
+            plugin_instance_list.append(data)
 
         cms_plugin_instance_list[placeholder.id] = plugin_instance_list
 
@@ -76,7 +116,7 @@ def _publish_receiver(sender, **kwargs):
         page_data= serialize('json', [ page_instance ]),
         placeholders= page_placeholders,
         plugins= cms_plugin_list,
-        plugin_instance = str(cms_plugin_instance_list),
+        plugin_instance = cms_plugin_instance_list,
     )
 
     version.save()
